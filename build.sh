2a6fe7b58348153d367743cde17da169ba8b0567
Pietro Lorefice <pietro.lorefice@gmail.com>
2016-02-13 19:50:37 +0100

    build.sh: add options to clean and build multiple entities

--- build.sh
+++ build.sh
@@ -2,11 +2,38 @@
 
 set -eu
 
-if [[ $# -lt 1 ]]; then
-	echo "Usage: build.sh <entity>"
-	exit 1
-fi
+DIR=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
+ENTS=()
+
+USAGE="$(basename "$0") [-h] [-c] [entities...] -- build and run the specified entities
+
+where
+	-h,--help	show this help text
+	-c,--clean	clean the object files"
+
+while [[ "$#" > 0 ]]; do
+key="$1"
+
+case $key in
+	-h|--help)
+	echo "$USAGE"
+	exit
+	;;
+	-c|--clean)
+	ghdl --clean --workdir=work
+	rm -f `find . -name "*.o"`
+	exit
+	;;
+	*)
+	ENTS+=($key)
+	;;
+esac
+shift
+done
 
 ghdl -i --workdir=work `find . -name "*.vhdl"`
-ghdl -m --workdir=work $1
-ghdl -r --workdir=work $1 --vcd="$1".vcd
+
+for ent in "${ENTS[@]}"; do
+	ghdl -m --workdir=work "$ent"
+	ghdl -r --workdir=work "$ent" --vcd="$ent".vcd
+done
