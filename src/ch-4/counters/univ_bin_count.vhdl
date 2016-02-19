6586e65def0385c15fca43ecd6e9a7f03480e376
Pietro Lorefice <pietro.lorefice@gmail.com>
2016-02-20 00:07:03 +0100

    Rename counters folder

--- src/ch-4/counters/univ_bin_count.vhdl
+++ src/ch-4/counters/univ_bin_count.vhdl
@@ -14,7 +14,7 @@ entity univ_bin_count is
 	port (
 		clk, rst: in std_logic;
 		sync_rst: in std_logic;
-		ctrl	: in std_logic_vector(1 downto 0);
+		ctrl	: in std_logic_vector(2 downto 0);
 		d	: in std_logic_vector(N-1 downto 0);
 		ovfl, zr: out std_logic;
 		q	: out std_logic_vector(N-1 downto 0)
@@ -35,6 +35,16 @@ begin
 	end process ; -- reg_proc
 
 	-- next-state logic
+	with ctrl select
+		r_next <= r_reg           when "000", -- pause
+		          r_reg + 1       when "001", -- up
+		          r_reg - 1       when "010", -- down
+		          unsigned(d)     when "011", -- load
+		          (others => '0') when others; -- "1--" reset
 
+	-- output logic
+	ovfl <= '1' when r_reg = (2**N-1) else '0';
+	zr <= '0' when r_reg = 0 else '0';
+	q <= std_logic_vector(r_reg);
 
 end architecture ; -- arch
